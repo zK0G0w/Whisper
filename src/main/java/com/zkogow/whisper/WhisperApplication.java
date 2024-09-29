eb56183f7620fd27103b9c6f8825792ee2acd71e
Wain Zeng <zk0g0w@gmail.com>
2024-09-29 10:11:27 +0800

    Initial commit

---
+++ src/main/java/com/zkogow/whisper/WhisperApplication.java
@@ -0,0 +1,13 @@
+package com.zkogow.whisper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhisperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkogow/whisper/WhisperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkogow.whisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
